018eeaa46810d87bea4c9c4bdd31d3ff7cbbcda3
Christian Hemprich <hemprich.c@gmail.com>
2026-08-20 21:14:30 -0700

    Add: flood-fill - submission-0

---
+++ Data Structures & Algorithms/flood-fill/submission-0.py
@@ -0,0 +1,25 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        original_color = image[sr][sc]
+
+        if original_color == color:
+            return image
+        
+        def dfs(r: int, c: int) -> None:
+            # base case out of matrix bounds
+            if r < 0 or c < 0 or r >= len(image) or c >= len(image[0]):
+                return
+
+            # base case different color from original
+            if image[r][c] != original_color:
+                return
+            
+            image[r][c] = color
+
+            dfs(r + 1, c)
+            dfs(r - 1, c)
+            dfs(r, c + 1)
+            dfs(r, c - 1)
+        
+        dfs(sr, sc)
+        return image
